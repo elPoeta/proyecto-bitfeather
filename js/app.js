059154a9e0f113ee2de770ff1933fe0d188011da
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2018-07-04 12:06:56 +0000

    Algunos cambios de estilos

--- js/app.js
+++ js/app.js
@@ -3,7 +3,9 @@ crearPost.addEventListener('click', (e) =>{
     template = `
     <section class="main-crear-post">
     <h2 class="titulo-main">Crear Post</h2>
+    <form>
     <input type="text" id="crear-titulo" placeholder="Titulo">
+    <input type="text" id="crear-subtitulo" placeholder="Subtitulo">
     <select>
         <option>Selecionar Categoria</option>
         <option value="cat1">Categoria 1</option>
@@ -11,11 +13,12 @@ crearPost.addEventListener('click', (e) =>{
         <option value="cat3">Categoria 3</option>
         <option value="cat4">Categoria 4</option>
       </select>
-<h3>Cuerpo:</h3>
+  <h3>Cuerpo:</h3>
     <textarea name="cuerpo" id="editor"></textarea>
     <h3>Tags:</h3>
     <textarea name="tag" rows="5"></textarea>
-    <button>Publicar</button>
+    <button id="btn-publicar-post">Publicar</button>
+    </form>
     </section>`;
     document.querySelector('#panel-contenido').innerHTML = template;
     ClassicEditor
