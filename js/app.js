4ba6df628483da091f881b4c5efc9779a2abe34f
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2018-07-08 19:14:56 +0000

    Desplegar categorias menu principal

--- js/app.js
+++ js/app.js
@@ -1,9 +1,21 @@
+const seleccionCategoriaNavbar = document.querySelector('#selecion-categoria-navbar');
+const menuHorizontalCat = document.querySelector('#menu-horizontal-cat');
 const btnNavPanel = document.querySelector('#btn-nav-panel');
 const btnNavIcon = document.querySelector('#nav-icon-hamburger');
 const navPanel = document.querySelector('.nav-panel');
 const crearPost = document.querySelector('#crear-post');
 const desplegarMenuCategoriasSidePanel = document.querySelector('#submenu-categorias');
 const subMenuCate = document.querySelector('#sub-menu-cat');
+
+seleccionCategoriaNavbar.addEventListener('click', (e)=>{
+    e.preventDefault();
+    if(menuHorizontalCat.classList.contains('hide-menu')){
+        menuHorizontalCat.classList.remove('hide-menu');
+    }
+    else{
+        menuHorizontalCat.classList.add('hide-menu');
+    }
+});
 
 btnNavIcon.addEventListener('click', (e)=>{
     e.preventDefault();
@@ -16,16 +28,16 @@ btnNavPanel.addEventListener('click', (e)=>{
     e.preventDefault();
     navPanel.style.width='0px';
     navPanel.style.border= '0px';
-    subMenuCate.classList.add('hide-submenu');
+    subMenuCate.classList.add('hide-menu');
 });
 
 desplegarMenuCategoriasSidePanel.addEventListener('click', (e) =>{
     e.preventDefault();
-    if(subMenuCate.classList.contains('hide-submenu')){
-        subMenuCate.classList.remove('hide-submenu');
+    if(subMenuCate.classList.contains('hide-menu')){
+        subMenuCate.classList.remove('hide-menu');
     }
     else{
-        subMenuCate.classList.add('hide-submenu');
+        subMenuCate.classList.add('hide-menu');
     }
 });
 
