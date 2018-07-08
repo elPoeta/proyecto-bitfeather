a441e2831102ed4d2194a5630a97cc9726a25f34
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2018-07-08 17:29:02 +0000

    Mejoras en menu lateral

--- js/app.js
+++ js/app.js
@@ -2,10 +2,12 @@ const btnNavPanel = document.querySelector('#btn-nav-panel');
 const btnNavIcon = document.querySelector('#nav-icon-hamburger');
 const navPanel = document.querySelector('.nav-panel');
 const crearPost = document.querySelector('#crear-post');
+const desplegarMenuCategoriasSidePanel = document.querySelector('#submenu-categorias');
+const subMenuCate = document.querySelector('#sub-menu-cat');
 
 btnNavIcon.addEventListener('click', (e)=>{
     e.preventDefault();
-   navPanel.style.width='300px';
+   navPanel.style.width='250px';
    navPanel.style.border= '3px solid #022037';
    navPanel.style.display='block';
 });
@@ -14,7 +16,19 @@ btnNavPanel.addEventListener('click', (e)=>{
     e.preventDefault();
     navPanel.style.width='0px';
     navPanel.style.border= '0px';
+    subMenuCate.classList.add('hide-submenu');
 });
+
+desplegarMenuCategoriasSidePanel.addEventListener('click', (e) =>{
+    e.preventDefault();
+    if(subMenuCate.classList.contains('hide-submenu')){
+        subMenuCate.classList.remove('hide-submenu');
+    }
+    else{
+        subMenuCate.classList.add('hide-submenu');
+    }
+});
+
 crearPost.addEventListener('click', (e) =>{
    Vista.mostrarCrearPost();
 });
