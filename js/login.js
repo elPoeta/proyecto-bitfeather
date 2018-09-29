65deb483cef3552709354d7d72b15018d609b3c5
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2018-09-29 20:21:13 -0300

    fix estilo login

--- js/login.js
+++ js/login.js
@@ -20,10 +20,9 @@ function sw1() {
     frontbox.classList.add("moving");
     signupMsg.classList.toggle("visibility");
   
-   // signup.classList.toggle('hide');
-    signup.classList.toggle('visibility');
-    //loginFront.classList.toggle('hide');
-    loginFront.classList.toggle('visibility');
+    signup.classList.toggle('hide');
+    loginFront.classList.toggle('hide');
+    
 }
 
 switch2.addEventListener('click', sw2);
@@ -33,10 +32,9 @@ function sw2() {
     frontbox.classList.remove("moving");
     signupMsg.classList.toggle("visibility");
   
-    //signup.classList.toggle('hide');
-    signup.classList.toggle('visibility');
-   // loginFront.classList.toggle('hide');
-    loginFront.classList.toggle('visibility');
+    signup.classList.toggle('hide');
+    loginFront.classList.toggle('hide');
+
   }
 //
 //setTimeout(function(){
